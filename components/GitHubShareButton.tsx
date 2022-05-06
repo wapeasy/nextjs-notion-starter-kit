c99565121677ab3cf661faefe5ee4a67f0cff1a3
wapeasy <ftp@msn.cn>
2022-05-06 15:50:22 +0800

    github share modify

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/wapeasy'
+      href='https://github.com/wapeasy/shdydgdheudjd-s-blog'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
